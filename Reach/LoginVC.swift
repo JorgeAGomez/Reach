878d7c40f6a95e6a58d568f77c0caf5f8f8c6d2e
Jorge <jorge_gomez12@hotmail.com>
2016-10-05 23:47:17 -0600

    Added Auth with facebook SDK

--- Reach/LoginVC.swift
+++ Reach/LoginVC.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 import FirebaseAuth
-//import FBSDKLoginKit
+import FBSDKLoginKit
 
 
 class LoginVC : UIViewController {
 
+  @IBOutlet weak var facebookButton: UIButton!
+  
   @IBOutlet weak var passwordTextField: UITextField!
   @IBOutlet weak var usernameTextField: UITextField!
   @IBOutlet weak var loginButton: UIButton!
-  @IBOutlet weak var signUpButton: UIButton!
-  
   let reachColor = UIColor(displayP3Red: 211/251, green: 81/251, blue: 67/251, alpha: 1.0).cgColor
   
   override func viewDidLoad() {
@@ -34,7 +34,38 @@ class LoginVC : UIViewController {
       self.tabBarController?.title = "Login"
       
   }
+  @IBAction func facebookButtonTapped(_ sender: AnyObject) {
+   
+       let facebookLogin = FBSDKLoginManager()
+    
+       facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+        if error != nil
+        {
+          print("Unable to authenticate with facebok!")
+        }
+        else if result?.isCancelled == true{
+          print("User cancelled facebook authentication")
+        }
+        else{
+          print("Facebook Authe Successfull")
+          let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+          self.firebaseAuth(credential)
+        }
+    }
+  }
 
+  func firebaseAuth(_ credential : FIRAuthCredential){
+    FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+      if error != nil{
+        print("Unable to authenticate with Firebase")
+      }
+      else{
+        print("Successfull authentication with Firebase")
+      }
+    })
+    
+  }
+  
   @IBAction func loginTapped(_ sender: AnyObject) {
     FIRAuth.auth()?.signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!) {(user, error )in
       if(error == nil){
